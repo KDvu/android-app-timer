4879d042414ec150f29e8ea2355b8627803425b1
Kenny Vu <kemyvu@gmail.com>
2016-09-15 13:42:35 +0100

    Positioned the TextViews relatively

---
+++ app/src/main/java/com/example/kdvu/timer/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.kdvu.timer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Layout
+        RelativeLayout rLayout = new RelativeLayout(this);
+
+        //TextViews
+        TextView topHeading = new TextView(this);
+        TextView hourHeading = new TextView(this);
+        TextView minHeading = new TextView(this);
+        TextView secHeading = new TextView(this);
+
+        //Set TextView text
+        topHeading.setText("Select Time");
+        topHeading.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 28);
+        hourHeading.setText("Hour");
+        minHeading.setText("Min");
+        secHeading.setText("Sec");
+
+        //Set TextView ids
+        topHeading.setId(R.id.topHeading);
+        hourHeading.setId(R.id.hourHeading);
+        minHeading.setId(R.id.minHeading);
+        secHeading.setId(R.id.secHeading);
+
+        //Set size of TextView
+        RelativeLayout.LayoutParams topHeadingDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams hourHeadingDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams minHeadingDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams secHeadingDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        //Create rules for positioning
+        topHeadingDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        minHeadingDetails.addRule(RelativeLayout.BELOW, topHeading.getId());
+        minHeadingDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        hourHeadingDetails.addRule(RelativeLayout.BELOW, topHeading.getId());
+        hourHeadingDetails.addRule(RelativeLayout.LEFT_OF, minHeading.getId());
+
+        secHeadingDetails.addRule(RelativeLayout.BELOW, topHeading.getId());
+        secHeadingDetails.addRule(RelativeLayout.RIGHT_OF, minHeading.getId());
+
+        rLayout.addView(topHeading, topHeadingDetails);
+        rLayout.addView(minHeading, minHeadingDetails);
+        rLayout.addView(hourHeading, hourHeadingDetails);
+        rLayout.addView(secHeading, secHeadingDetails);
+
+        setContentView(rLayout);
+    }
+}
